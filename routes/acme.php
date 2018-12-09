c3ae8a218e27155b754497fb40f83cc24bcae699
xiaohui.lam <xiaohui.lam@icloud.com>
2018-12-10 00:44:37 +0800

    fix route

--- routes/acme.php
+++ routes/acme.php
@@ -12,9 +12,12 @@ use Illuminate\Http\Request;
 
 Route::get('/directory', 'DirectoryController@index')->name('acme.directory');
 
+// routes appeared in the directory api response
 Route::post('/acme/key-change', 'KeyController@change')->name('acme.key.change');
-Route::any('/acme/new-acct', 'AccountController@new')->name('acme.account.new');
-Route::any('/acme/acct/{id}', 'AccountController@new')->name('acme.account.detail');
+Route::post('/acme/new-acct', 'AccountController@new')->name('acme.account.new');
 Route::post('/acme/new-nonce', 'NonceController@new')->name('acme.nonce.new');
 Route::post('/acme/new-order', 'OrderController@new')->name('acme.order.new');
 Route::post('/acme/revoke-cert', 'CertController@new')->name('acme.cert.revoke');
+
+// route not appeared in the directory api response
+Route::post('/acme/acct/{id}', 'AccountController@new')->name('acme.account.detail');
