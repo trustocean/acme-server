22c1e9eea3b1bb31903634813a8d9d65ac51c08f
xiaohui.lam <xiaohui.lam@icloud.com>
2019-07-23 22:17:46 +0800

    create account table

--- database/migrations/2019_07_23_133139_create_acme_accounts.php
+++ database/migrations/2019_07_23_133139_create_acme_accounts.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAcmeAccounts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('acme_accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email', 64)->index();
+            $table->string('thumb_print', 40)->index();
+            $table->text('public_key');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('acme_accounts');
     }
 }
