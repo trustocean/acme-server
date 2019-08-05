a1c1616cf356cdbccd4c18add9ecb70e58c106f1
xiaohui.lam <xiaohui.lam@icloud.com>
2019-08-05 14:57:44 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,6 +77,133 @@
                 width: 98px;
                 height: 36px;
             }
+            body {
+                margin: 0
+            }
+
+            a {
+                color: #24292e;
+                text-decoration: none;
+                outline: 0
+            }
+
+            .octicon {
+                display: inline-block;
+                vertical-align: text-top;
+                fill: currentColor
+            }
+
+            .widget {
+                display: inline-block;
+                overflow: hidden;
+                font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
+                font-size: 0;
+                white-space: nowrap;
+                -webkit-user-select: none;
+                -moz-user-select: none;
+                -ms-user-select: none;
+                user-select: none
+            }
+
+            .btn, .social-count {
+                display: inline-block;
+                height: 14px;
+                padding: 2px 5px;
+                font-size: 11px;
+                font-weight: 600;
+                line-height: 14px;
+                vertical-align: bottom;
+                cursor: pointer;
+                border: 1px solid #c5c9cc;
+                border-radius: 0.25em
+            }
+
+            .btn {
+                background-color: #eff3f6;
+                background-image: -webkit-linear-gradient(top, #fafbfc, #eff3f6 90%);
+                background-image: -moz-linear-gradient(top, #fafbfc, #eff3f6 90%);
+                background-image: linear-gradient(180deg, #fafbfc, #eff3f6 90%);
+                background-position: -1px -1px;
+                background-repeat: repeat-x;
+                background-size: 110% 110%;
+                border-color: rgba(27, 31, 35, 0.2);
+                -ms-filter: "progid:DXImageTransform.Microsoft.Gradient(startColorstr='#FFFAFBFC', endColorstr='#FFEEF2F5')";
+                *filter: progid:DXImageTransform.Microsoft.Gradient(startColorstr='#FFFAFBFC', endColorstr='#FFEEF2F5')
+            }
+
+            .btn:active {
+                background-color: #e9ecef;
+                background-image: none;
+                border-color: #a5a9ac;
+                border-color: rgba(27, 31, 35, 0.35);
+                box-shadow: inset 0 0.15em 0.3em rgba(27, 31, 35, 0.15)
+            }
+
+            .btn:focus, .btn:hover {
+                background-color: #e6ebf1;
+                background-image: -webkit-linear-gradient(top, #f0f3f6, #e6ebf1 90%);
+                background-image: -moz-linear-gradient(top, #f0f3f6, #e6ebf1 90%);
+                background-image: linear-gradient(180deg, #f0f3f6, #e6ebf1 90%);
+                border-color: #a5a9ac;
+                border-color: rgba(27, 31, 35, 0.35);
+                -ms-filter: "progid:DXImageTransform.Microsoft.Gradient(startColorstr='#FFF0F3F6', endColorstr='#FFE5EAF0')";
+                *filter: progid:DXImageTransform.Microsoft.Gradient(startColorstr='#FFF0F3F6', endColorstr='#FFE5EAF0')
+            }
+
+            .social-count {
+                position: relative;
+                margin-left: 5px;
+                background-color: #fff
+            }
+
+            .social-count:focus, .social-count:hover {
+                color: #0366d6
+            }
+
+            .social-count b, .social-count i {
+                position: absolute;
+                top: 50%;
+                left: 0;
+                display: block;
+                width: 0;
+                height: 0;
+                margin: -4px 0 0 -4px;
+                border: solid transparent;
+                border-width: 4px 4px 4px 0;
+                _line-height: 0;
+                _border-top-color: red !important;
+                _border-bottom-color: red !important;
+                _border-left-color: red !important;
+                _filter: chroma(color=red)
+            }
+
+            .social-count b {
+                border-right-color: #c5c9cc
+            }
+
+            .social-count i {
+                margin-left: -3px;
+                border-right-color: #fff
+            }
+
+            .lg .btn, .lg .social-count {
+                height: 28px;
+                padding: 5px 10px;
+                font-size: 12px;
+            }
+
+            .lg .social-count {
+                margin-left: 6px
+            }
+
+            .lg .social-count b, .lg .social-count i {
+                margin: -5px 0 0 -5px;
+                border-width: 5px 5px 5px 0
+            }
+
+            .lg .social-count i {
+                margin-left: -4px
+            }
         </style>
     </head>
     <body class="container">
@@ -87,8 +214,28 @@
                     <span style="width:60%">ACME</span>
                 </div>
                 <p>
-                    <a class="github-button" href="https://github.com/trustocean" data-size="large" data-show-count="true" aria-label="Follow @trustocean on GitHub">Follow @trustocean</a>
-                    <a class="github-button" href="https://github.com/trustocean/acme-server" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star trustocean/acme-client on GitHub">Star</a>
+                    <span class="widget lg">
+                        <a class="btn" href="https://github.com/trustocean" target="_blank" aria-label="Follow @trustocean  on GitHub">
+                            <svg version="1.1" width="16" height="16" viewBox="0 0 16 16" class="octicon octicon-mark-github" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
+                            <span>Follow @trustocean</span>
+                        </a>
+                        <a class="social-count" href="https://github.com/trustocean/followers" target="_blank" aria-label="0 follower on GitHub">
+                            <b></b>
+                            <i></i>
+                            <span id="followers">NaN</span>
+                        </a>
+                    </span>
+                    <span class="widget lg">
+                        <a class="btn" href="https://github.com/trustocean/acme-server" target="_blank" aria-label="Star trustocean/acme-client on GitHub">
+                            <svg version="1.1" width="14" height="16" viewBox="0 0 14 16" class="octicon octicon-star" aria-hidden="true"><path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path></svg>
+                            <span>Star</span>
+                        </a>
+                        <a class="social-count" href="https://github.com/trustocean/acme-server/stargazers" target="_blank" aria-label="1 stargazer on GitHub">
+                            <b></b>
+                            <i></i>
+                            <span id="stargazers_count">NaN</span>
+                        </a>
+                    </span>
                 </p>
                 <h2 class="text-left">安装使用 <small>目前不支持其他 ACME 客户端</small></h2><br/>
                 <pre class="text-left">
@@ -162,6 +309,15 @@ certificates:
 </pre>
             </div>
         </div>
-        <script async defer src="https://buttons.github.io/buttons.js"></script>
+        <script src="https://lib.sinaapp.com/js/jquery/1.12.4/jquery-1.12.4.min.js"></script>
+        <script>
+            $.getJSON('https://api.github.com/repos/trustocean/acme-server', function (json) {
+                $('#stargazers_count').text(json.stargazers_count);
+            });
+            $.getJSON('https://api.github.com/users/trustocean', function (json) {
+                $('#followers').text(json.followers);
+            });
+            
+        </script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('cache.headers:public;max_age=2628000;etag');
